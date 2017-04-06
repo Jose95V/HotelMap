6e0bfee95069e09ff99e992ae57cc5bee33ff379
Jose Vallecillo <jose.vallecillo@ujcv.edu.hn>
2017-04-06 13:35:01 -0600

    modificando nombres del navigation

--- app/src/main/java/com/example/berenice/proyectohoteles/MainActivity.java
+++ app/src/main/java/com/example/berenice/proyectohoteles/MainActivity.java
@@ -31,14 +31,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -94,9 +86,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            fm.beginTransaction().replace(R.id.content_frame, new ImportFragment()).commit();
-        } else if (id == R.id.nav_gallery) {
             fm.beginTransaction().replace(R.id.content_frame, new GmapFragment()).commit();
+        } else if (id == R.id.nav_gallery) {
+            fm.beginTransaction().replace(R.id.content_frame, new ImportFragment()).commit();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
